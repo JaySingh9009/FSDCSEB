8f52b96037bb01e3be4c0e63f065e025cd996598
JaySingh9009 <singh002jay@gmail.com>
2024-10-07 15:32:41 +0530

    grid work amazon page

--- image/datetime.js
+++
@@ -1,6 +0,0 @@
-var d = new Date();
-var current_date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+ d.getDate();
-document.getElementById("p1").innerHTML = current_date;
-var date = new Date();
-var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
-document.getElementById("p2").innerHTML = current_time;
